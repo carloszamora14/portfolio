4b607b046fc24a3484aa5fe3155ea16cc78b87ab
Carlos Zamora <carlos14zamora@gmail.com>
2024-09-05 14:55:43 -0600

    modify skillsList and navigationLinks data to add missing details

--- src/data/navigationLinks.js
+++ src/data/navigationLinks.js
@@ -1,6 +1,7 @@
-export const sections = [
+export const navigationLinks = [
   { url: '#home', name: 'Home' },
   { url: '#projects', name: 'Projects' },
+  { url: '#About', name: 'About me' },
   { url: '#skills', name: 'Skills' },
   { url: '#contact', name: 'Contact' },
 ];

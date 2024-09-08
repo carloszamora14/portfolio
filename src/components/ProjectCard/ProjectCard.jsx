cd007b5b95266eebe2eeeb41127ea646ed901798
Carlos Zamora <carlos14zamora@gmail.com>
2024-09-08 10:51:38 -0600

    modify CSS styles for NavMobile, Project and ProjectCard components

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,12 +1,22 @@
 import styles from './ProjectCard.module.css';
 
-function ProjectCard() {
+function ProjectCard({ img, imgAlt, projectName, description }) {
   return (
-    <div className={styles.projectCard}>
-      <img src="project1.jpg" alt="Project 1" />
-      <h3>Project Title 1</h3>
-      <p>Short description of the project.</p>
-    </div>
+    <article className={styles.projectCard}>
+      <div className={styles.imageWrapper}>
+        <img src={img} alt={imgAlt} className={styles.projectImage} />
+      </div>
+      <section className={styles.projectDetails}>
+        <h3 className={styles.projectTitle}>{projectName}</h3>
+        <p className={styles.projectDescription}>{description}</p>
+        <button
+          className={styles.projectButton}
+          aria-label={`View ${projectName} project`}
+        >
+          View project
+        </button>
+      </section>
+    </article>
   );
 }
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,13 @@
+import ProjectCard from '../ProjectCard/ProjectCard';
 import SectionHeader from '../SectionHeader/SectionHeader';
 import styles from './Projects.module.css';
+import Lumina from '../../assets/lumina/login.jpeg';
+import DailyUiChallenge from '../../assets/daily-ui-challenge/cover.png';
+import Emoiq from '../../assets/emoiq/cover.png';
+import OhMyDeer from '../../assets/oh-my-deer/2.png';
+import NumberGuessingGame from '../../assets/number-guessing-game/cover.png';
+import DonaBikes from '../../assets/dona-bikes/cover.jpg';
+import MovieFinder from '../../assets/movie-finder/cover.png';
 
 function Projects() {
   return (
@@ -15,7 +23,57 @@ function Projects() {
           I&apos;ve acquired and my dedication to learning and improving with
           every challenge.
         </p>
-        <div className={styles.content}></div>
+        <div className={styles.content}>
+          <ProjectCard
+            img={Lumina}
+            imgAlt="Mobile application Lumina home screen"
+            projectName="Lumina"
+            tagline="Be safe"
+            description="A project with social benefit"
+          />
+          <ProjectCard
+            img={DailyUiChallenge}
+            imgAlt="Mobile application Lumina home screen"
+            projectName="Daily UI Challenge"
+            tagline="Be safe"
+            description="A project with social benefit"
+          />
+          <ProjectCard
+            img={Emoiq}
+            imgAlt="Mobile application Lumina home screen"
+            projectName="EmoIQ"
+            tagline="Be safe"
+            description="A project with social benefit"
+          />
+          <ProjectCard
+            img={OhMyDeer}
+            imgAlt="Mobile application Lumina home screen"
+            projectName="Oh My Deer"
+            tagline="Be safe"
+            description="A project with social benefit"
+          />
+          <ProjectCard
+            img={NumberGuessingGame}
+            imgAlt="Mobile application Lumina home screen"
+            projectName="Number guessing game"
+            tagline="Be safe"
+            description="A project with social benefit"
+          />
+          <ProjectCard
+            img={DonaBikes}
+            imgAlt="Mobile application Lumina home screen"
+            projectName="Dona Bikes"
+            tagline="Be safe"
+            description="A project with social benefit"
+          />
+          <ProjectCard
+            img={MovieFinder}
+            imgAlt="Mobile application Lumina home screen"
+            projectName="Movie Finder"
+            tagline="Be safe"
+            description="A project with social benefit"
+          />
+        </div>
       </div>
     </section>
   );

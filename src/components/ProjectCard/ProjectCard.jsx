8d0240a8cc2b9e3450227cd0d067eb83ebc4dfe5
Carlos Zamora <carlos14zamora@gmail.com>
2024-09-27 23:20:45 -0600

    refactor markup of ProjectCard component and update styles for Project and ProjectCard

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -3,21 +3,23 @@ import styles from './ProjectCard.module.css';
 
 function ProjectCard({ img, imgAlt, projectName, description, url }) {
   return (
-    <article className={styles.projectCard}>
-      <div className={styles.imageWrapper}>
-        <img src={img} alt={imgAlt} className={styles.projectImage} />
-      </div>
-      <section className={styles.projectDetails}>
-        <h3 className={styles.projectTitle}>{projectName}</h3>
-        <p className={styles.projectDescription}>{description}</p>
-        <Link
-          to={url}
-          className={styles.projectButton}
-          aria-label={`View ${projectName} project`}
-        >
-          View project
-        </Link>
-      </section>
+    <article className={styles.card}>
+      <figure className={styles.figure}>
+        <div className={styles.imageWrapper}>
+          <img src={img} alt={imgAlt} className={styles.image} />
+        </div>
+        <figcaption className={styles.details}>
+          <h3 className={styles.title}>{projectName}</h3>
+          <p className={styles.description}>{description}</p>
+          <Link
+            to={url}
+            className={styles.button}
+            aria-label={`Learn more about the ${projectName} project`}
+          >
+            Read more
+          </Link>
+        </figcaption>
+      </figure>
     </article>
   );
 }
--- src/data/projects.js
+++ src/data/projects.js
@@ -19,7 +19,6 @@ const projects = [
     img: DailyUiChallenge,
     imgAlt: 'Showcase of various mobile app UI designs',
     projectName: 'Daily UI Challenge',
-    tagline: 'Daily design practice',
     description:
       'A collection of daily UI designs aimed at improving my design skills and creativity.',
     url: '/daily-ui-challenge',
@@ -28,7 +27,6 @@ const projects = [
     img: Emoiq,
     imgAlt: 'Home screen of the EmoIQ mobile application',
     projectName: 'EmoIQ',
-    tagline: 'Enhance your emotional intelligence',
     description:
       'A mobile app focused on learning and improving emotional intelligence through engaging activities.',
     url: '/emoiq',
@@ -37,7 +35,6 @@ const projects = [
     img: OhMyDeer,
     imgAlt: 'Silhouette of a deer on a mountain backdrop',
     projectName: 'Oh My Deer',
-    tagline: 'Raise awareness against animal hunting',
     description:
       'A 2D adventure game that raises awareness about the impact of animal hunting on wildlife.',
     url: '/oh-my-deer',
@@ -46,7 +43,6 @@ const projects = [
     img: DonaBikes,
     imgAlt: 'Mockup of a smartphone displaying the Dona Bikes app home screen',
     projectName: 'Dona Bikes',
-    tagline: 'Explore your biking adventure',
     description:
       'A design and prototype created in Figma for a fictional bike shop.',
     url: '/dona-bikes',
@@ -55,7 +51,6 @@ const projects = [
     img: MovieFinder,
     imgAlt: 'Home screen of the Movie Finder application',
     projectName: 'Movie Finder',
-    tagline: 'Discover your next favorite film',
     description:
       'A React web application that allows users to search for and display information about movies.',
     url: '/movie-finder',
